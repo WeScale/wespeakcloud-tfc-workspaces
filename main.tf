afe3030db2027d3b4a20e0d0109071ac7ac85a64
Benoît OYEZ <benoit.oyez@wescale.fr>
2020-12-04 17:15:27 +0000

    Introducing github_basic_workspaces

--- main.tf
+++ main.tf
@@ -1,19 +1,58 @@
 terraform {
   required_providers {
-    tfe = "~> 0.15.0"
+    tfe = "~> 0.23.0"
+  }
+
+  backend "remote" {
+    organization = "wescalefr"
+
+    workspaces {
+      name = "wespeakcloud-tfc-workspaces"
+    }
   }
 }
 
 locals {
-  dummy_project_name = "wespeakcloud-dummy-tf"
+  github_basic_workspaces = {
+    for workspace in var.github_basic_workspaces:
+    workspace.name => merge(workspace, {
+      oauth_token_id = tfe_oauth_client.github_wescale.oauth_token_id
+      identifier = "WeScale/${workspace.identifier}"
+      organization_id = var.organization_id
+    })
+  }
 }
 
-module "dummy" {
+resource "tfe_oauth_client" "github_wescale" {
+  organization = var.organization_id
+  api_url = "https://api.github.com"
+  http_url = "https://github.com"
+  oauth_token = var.github_token
+  service_provider = "github"
+}
+
+resource "tfe_policy_set" "policies" {
+  name = var.policies_repo_name
+  organization = var.organization_id
+  global = false
+  workspace_external_ids = keys(local.github_basic_workspaces)
+
+  vcs_repo {
+    identifier = "WeScale/${var.policies_repo_name}"
+    branch = var.policies_repo_branch
+    oauth_token_id = tfe_oauth_client.github_wescale.oauth_token_id
+  }
+}
+
+module "basic_workspaces" {
   source  = "app.terraform.io/wescalefr/basic_workspace/tfe"
   version = "0.1.0"
+  
+  for_each = local.github_basic_workspaces
 
-  name = local.dummy_project_name
-  organization_id = var.organization_id
-  oauth_token_id = var.github_oauth_token_id
-  vcs_repo_identifier = "WeScale/${local.dummy_project_name}"
+  name = each.value.name
+  organization_id = each.value.organization_id
+  vcs_repo_identifier = each.value.identifier
+  oauth_token_id = each.value.oauth_token_id
 }
+
--- variables.tf
+++ variables.tf
@@ -1,9 +1,34 @@
-variable "github_oauth_token_id" {
+variable "github_token" {
   type = string
-  description = "Oauth token ID used in the workspaces connection to github.com"
+  description = "Token to connect TFC and github.com"
 }
 
 variable "organization_id" {
   type = string
   description = "Name of the organization hosting the workspaces"
+}
+
+variable "policies_repo_name" {
+  type = string
+  description = "Name of the policies repository in github"
+}
+
+variable "policies_path" {
+  type = string
+  description = "Path of the policies in the project"
+  default = "."
+}
+
+variable "policies_repo_branch" {
+  type = string
+  description = "Branch of the policies repository"
+  default = "main"
+}
+
+variable "github_basic_workspaces" {
+  type = list(object({
+    name = string
+    identifier = string
+  }))
+  description = "List of basic Github workspaces"
 }
